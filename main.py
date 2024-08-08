29fe3cd31f452be7b16841f828c8b6508abc1162
HeavenMercy <gamedevrichard@gmail.com>
2024-08-08 22:06:38 +0100

    make many modifications
    update prompt_template
    
    fix no embedding in the veccctor database

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -9,22 +11,35 @@ from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
 load_dotenv()
 
+DB_PATH = './db'
+
+# --------------------------------------------------------------------------------
+
 with open('data/prompt_template.txt', 'r') as f:
     prompt_template = f.read()
 
 prompt = PromptTemplate(template=prompt_template, input_variables=['question'])
 
-pdfloader = PyPDFLoader('data/fashion_data.pdf')
-documents = pdfloader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=2000, chunk_overlap=300)
-texts = text_splitter.split_documents(documents)
-
 embed_model = HuggingFaceEmbeddings(
     model_name='sentence-transformers/all-MiniLM-L6-v2')
 
-vector_db = Chroma(embedding_function=embed_model, persist_directory='db')
+create_db = not os.path.exists(DB_PATH)
+
+vector_store = Chroma(embedding_function=embed_model,
+                      persist_directory=DB_PATH)
+
+if create_db:
+    pdfloader = PyPDFLoader('data/fashion_data.pdf')
+    documents = pdfloader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000, chunk_overlap=300)
+    documents = text_splitter.split_documents(documents)
+
+    vector_store.add_documents(documents)
+    print('[vector database created]')
+else:
+    print('[vector database already exist]')
 
 model = HuggingFaceEndpoint(repo_id='mistralai/Mixtral-8x7B-Instruct-v0.1',
                             temperature=1, max_new_tokens=1024)
@@ -32,20 +47,20 @@ model = HuggingFaceEndpoint(repo_id='mistralai/Mixtral-8x7B-Instruct-v0.1',
 rag_chain = RetrievalQA.from_chain_type(
     llm=model,
     chain_type='stuff',
-    retriever=vector_db.as_retriever(top_k=3),
+    retriever=vector_store.as_retriever(top_k=3),
     chain_type_kwargs={'prompt': prompt})
 
 
 def get_answer(question):
-    # last_in_prompt = 'Answer:'
-
     result = rag_chain.invoke({'query': question})
-    result = result['result']
-    # end_prompt = result.index(last_in_prompt) + len(last_in_prompt)
+    result = result['result'].strip()
 
-    # result = result[end_prompt:].strip()
     return result
 
 
 if __name__ == '__main__':
-    print(get_answer("What can you recommend that is made off of cashmere?"))
+    while True:
+        question = input('>> ')
+        if question.lower() in ['exit', 'quit']:
+            break
+        print(get_answer(question))
